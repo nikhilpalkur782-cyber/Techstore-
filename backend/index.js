ac3589bad77a10399b574df3f578f86aac2882d8
Gurushant92 <gurushant716@gmail.com>
2025-12-11 12:53:27 +0530

    initial commit 1

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Utiles
 import connectDB from "./config/db.js";
@@ -19,6 +20,14 @@ connectDB();
 
 const app = express();
 
+// CORS configuration for production
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "*",
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
